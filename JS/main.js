ce6090e9505c276cab67f4c085165237ff07ef7d
MustafaHamdy10 <mustafahamdy1010@gmail.com>
2024-12-18 20:33:09 +0200

    first commit

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+// Initialization of variables .
+
+let panels = document.querySelectorAll(".panel");
+
+// Make a function to iterate over each panel
+
+panels.forEach((panel) => {
+  panel.addEventListener("click", () => {
+    removeActiveClasses();
+    panel.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  panels.forEach((panel) => {
+    panel.classList.remove("active");
+  });
+}
